4a13ebab54a473dc047dca45fd5c1b6d12e1d41a
arikakira <samanthal242@nycstudents.net>
2025-10-20 20:13:41 +0000

    part 1 done

--- Main.py
+++ Main.py
@@ -1,1 +1,11 @@
+def clampValueToRange(value, low, high) :
+    if value < low :
+        return int(low)
+    elif value > high :
+        return int(high)
+    else :
+        return int(value)
 
+print(clampValueToRange(234.5, 0, 255))
+print(clampValueToRange(-10, 100.5, 200))
+print(clampValueToRange(500, 0, 200))
