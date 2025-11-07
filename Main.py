eac0ba487199d20a71eef376e00b8834a9bc7b2b
arikakira <samanthal242@nycstudents.net>
2025-11-07 02:17:17 +0000

    milestone 2

--- Main.py
+++ Main.py
@@ -23,6 +23,36 @@ def returnLocation(line) :
 def locationValid(x, width, y, height) :
     return (x >= 0 and y >= 0) and (x < width and y < height)
 
+def convertPixel(color) :
+    result = {}
+    nums = []
+    for i in range(len(color)) :
+        val = clampValueToRange(color[i], 0, 255)
+        nums.append(val)
+    result["r"] = nums[0]
+    result["g"] = nums[1]
+    result["b"] = nums[2]
+    print(nums)
+    return result
+
+def positionPixel(x, y, color) :
+    result = {}
+    result["pixel"] = color
+    result["x"] = x
+    result["y"] = y
+    return result
+
+def updateChangeList(pixel, pixelList) :
+    pixelList.append(pixel)
+
+def readPixelFile(pixels, filename) :
+    with open(filename, "r") as info :
+        for line in info :
+            rgbList = returnColor(line)
+            rgbDict = convertPixel(rgbList)
+            xANDy = returnLocation(line)
+            updateChangeList(positionPixel(xANDy[0], xANDy[1], rgbDict), pixels)
+
 print(clampValueToRange(234.5, 0, 255))
 print(clampValueToRange(-10, 100.5, 200))
 print(clampValueToRange(500, 0, 200))
@@ -35,3 +65,15 @@ print(locationValid(-1, 10, 0, 20))
 print(locationValid(20, 10, 0, 20))
 print(locationValid(-5, 10, 5, 20))
 print(locationValid(0, 10, 2, 20))
+
+print(convertPixel([-10, 100.5, 300]))
+print(convertPixel([500, -5, 25.5]))
+
+print(positionPixel(2, 3, {'r': 0, 'g': 100, 'b': 255}))
+
+updateChangeList({'pixel': {'r': 0, 'g': 100, 'b': 255}, 'x': 2, 'y': 3}, [{'pixel': {'r': 1, 'g': 2, 'b': 3}, 'x': 4, 'y': 5}])
+
+pixels = []
+readPixelFile(pixels, "example.txt")
+print("TESTING JGBUIAORHIPWOHFUIQOWIHDOIQHDBOIHQDIO")
+print(pixels)
